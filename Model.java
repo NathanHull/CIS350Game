2ad23538d92319ac19887f464f3c53aee590e59d
Matthew Pairitz <mattpair@users.noreply.github.com>
2016-09-26 00:51:51 -0400

    Update Model.java

--- Model.java
+++ Model.java
@@ -1,14 +1,22 @@
+import java.util.*;
+
 public class Model{
 	
-	private ArrayList houses = new ArrayList<House>();
-	
+	private ArrayList houseList = new ArrayList<House>();
 	
 	public Model(){
 		
 	}
 	
 	
-	private void updatePrices(ArrayList<House> houses){
+	private void updatePrices(ArrayList<House> houseList){
+		int minTier0=100;
+		int maxTier0=200;
+		int minTier1=500;
+		int maxTier1=2000;
+		int minTier2=10000;
+		int maxTier2=25000;
+		int index;
 		/**
 		tier 0- pricey sheds; price min/max: 800-1,500
 		add tier 2 houses at $10,000
@@ -22,6 +30,36 @@ public class Model{
 		generated index which is determined
 		by the house tier
 		**/
+		Random random = new Random();
+		
+		for(house h : houses){
+		     if(h.getTier()==0){
+			 index=random.nextInt((maxTier0 - minTier0) + 1) + minTier0;
+			 if(random.nextBoolean){
+			     h.setPrice(h.getPrice+=index);
+			 } else {
+			     h.setPrice(h.getPrice-=index);	 
+			 }
+		     }
+			
+		     if(h.getTier()==1){
+			 index=random.nextInt((maxTier1 - minTier1) + 1) + minTier1;
+			 if(random.nextBoolean){
+			     h.setPrice(h.getPrice+=index);
+			 } else {
+			     h.setPrice(h.getPrice-=index);	 
+			 }
+		     }
+			
+		     if(h.getTier()==2){
+			 index=random.nextInt((maxTier2 - minTier2) + 1) + minTier2;
+			 if(random.nextBoolean){
+			     h.setPrice(h.getPrice+=index);
+			 } else {
+			     h.setPrice(h.getPrice-=index);	 
+			 }
+		     }
+		}
 	}
 	
 	/** public void buyHouse(){
