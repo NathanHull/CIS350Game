718a3ed7ee96adf774654f6ce227de2f181177d7
mattpair <mpairitz@gmail.com>
2016-09-26 00:07:31 -0400

    Model class and House Class skeleton

---
+++ Model.java
@@ -0,0 +1,36 @@
+public class Model{
+	
+	private ArrayList houses = new ArrayList<House>();
+	
+	
+	public Model(){
+		
+	}
+	
+	
+	private void updatePrices(ArrayList<House> houses){
+		/**
+		tier 1- pricey sheds; price min/max: 800-1,500
+		add tier 2 houses at $10,000
+		tier 2- the vanishing American middle class; price min/max: 2,000-10,000
+		add tier 3 houses at $100,000
+		tier 3- Robber Barons; price min/max: 50,000-100,000
+		Goal: 1 million
+		
+		implement by iterating through houses 
+		adding either a static or dynamically
+		generated index which is determined
+		by the house tier
+		**/
+	}
+	
+	/** public void buyHouse(){
+		change ownership and subtract price from player money
+		if not enough money then don't do it
+	}
+	
+	public void sellHouse(){
+		above, vice versa
+	}
+	**/
+}
