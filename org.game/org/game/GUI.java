c54c25de1a71df36836de672fb4fdafc62e83633
Justin Wickenheiser <wickenhj@gmail.com>
2016-12-06 20:15:34 -0500

    Tried to make check style happy. Broke something in the process

--- org.game/org/game/GUI.java
+++ org.game/org/game/GUI.java
@@ -1,14 +1,14 @@
 package org.game;
-import java.awt.EventQueue;
+//import java.awt.EventQueue;
 import java.awt.BorderLayout;
 import javax.swing.JFrame;
-import javax.swing.JTextArea;
-import java.awt.Graphics;
-import java.awt.Image;
+//import javax.swing.JTextArea;
+//import java.awt.Graphics;
+//import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-import java.io.File;
+//import java.awt.image.BufferedImage;
+//import java.io.File;
 import java.io.IOException;
 import java.text.DecimalFormat;
 
@@ -17,10 +17,12 @@ import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
-import javax.imageio.ImageIO;
+//import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JTextField;
+//import javax.swing.JTextField;
+
+
 
 /**
  * 
@@ -28,15 +30,51 @@ import javax.swing.JTextField;
  *
  */
 public class GUI extends Init implements ActionListener {
-
 	/**
-	 * JFrame
+	 * THREE.
+	 */
+	public static final int THREE = 3;
+	/**
+	 * FOUR.
+	 */
+	public static final int FOUR = 4;
+	/**
+	 * FIVE.
+	 */
+	public static final int FIVE = 5;
+	/**
+	 * TEN.
+	 */
+	public static final int TEN = 10;
+	/**
+	 * ONE_HUNDRED.
+	 */
+	public static final int ONE_HUNDRED = 100;
+	/**
+	 * FOUR_FIFTY.
+	 */
+	public static final int FOUR_FIFTY = 450;
+	/**
+	 * FIVE_HUNDRED.
+	 */
+	public static final int FIVE_HUNDRED = 500;
+	
+	
+	
+	
+	/**
+	 * JFrame.
 	 */
 	private JFrame frame;
 	/**
 	 * JButton.
 	 */
-	private JButton buyHouse0, sellHouse0, buyHouse1, sellHouse1, buyHouse2, sellHouse2, buyHouse3, sellHouse3, buyHouse4, sellHouse4, buyHouse5, sellHouse5;
+	private JButton buyHouse0, sellHouse0, 
+		buyHouse1, sellHouse1, 
+		buyHouse2, sellHouse2, 
+		buyHouse3, sellHouse3, 
+		buyHouse4, sellHouse4, buyHouse5, 
+		sellHouse5;
 	/**
 	 * JMenuBar.
 	 */
@@ -48,7 +86,7 @@ public class GUI extends Init implements ActionListener {
 	/**
 	 * JMenuItem.
 	 */
-	private JMenuItem OptionsItem, SysItem;
+	private JMenuItem optionsItem, sysItem;
 	/**
 	 * Model.
 	 */
@@ -58,13 +96,13 @@ public class GUI extends Init implements ActionListener {
 	 */
 	private JLabel lblWallet;
 	/**
-	 * Labels for house prices
+	 * Labels for house prices.
 	 */
 	private JLabel lblHP0, lblHP1, lblHP2, lblHP3, lblHP4, lblHP5;
 	/**
-	 * Decimal format for labels
+	 * Decimal format for labels.
 	 */
-	DecimalFormat df = new DecimalFormat("##.00");
+	private DecimalFormat df = new DecimalFormat("##.00");
 	
 	/**
 	 * Create the application.
@@ -91,201 +129,202 @@ public class GUI extends Init implements ActionListener {
 		menuBar.setVisible(true);
 
 		sys = new JMenu("System");
-		SysItem = new JMenuItem("Reset");
-		SysItem.addActionListener(this);
-		SysItem.setActionCommand("reset");
-		sys.add(SysItem);
-		SysItem = new JMenuItem("Quit");
-		SysItem.addActionListener(this);
-		SysItem.setActionCommand("quit");
-		sys.add(SysItem);
+		sysItem = new JMenuItem("Reset");
+		sysItem.addActionListener(this);
+		sysItem.setActionCommand("reset");
+		sys.add(sysItem);
+		sysItem = new JMenuItem("Quit");
+		sysItem.addActionListener(this);
+		sysItem.setActionCommand("quit");
+		sys.add(sysItem);
 		
 		options = new JMenu("Options");
-		OptionsItem = new JMenuItem("Settings");
-		OptionsItem.addActionListener(this);
-		OptionsItem.setActionCommand("settings");
-		options.add(OptionsItem);
+		optionsItem = new JMenuItem("Settings");
+		optionsItem.addActionListener(this);
+		optionsItem.setActionCommand("settings");
+		options.add(optionsItem);
 		
 		menuBar.add(options);
 		menuBar.add(sys);
 		getFrame().setJMenuBar(menuBar);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(0, 307, 434, 36);
+		JPanel panel1 = new JPanel();
+		panel1.setBounds(0, 307, 434, 36);
 		
 		JPanel panel = new JPanel();
-		panel.setBounds(10, 21, 414, 145);
+		panel.setBounds(TEN, 21, 414, 145);
 		
 		ImageIcon image = new ImageIcon("Game background resized.jpg");
 		JLabel label = new JLabel("", image, JLabel.CENTER);
 		panel.add(label, BorderLayout.CENTER);
 		lblWallet = new JLabel("Wallet: $" + df.format(model.getWallet()));
-		JLabel copyright = new JLabel("Version 1.0: Created By TBD. Copyright 2016 \u00a9");
+		JLabel copyright = new JLabel(
+				"Version 1.0: Created By TBD. Copyright 2016 \u00a9");
 		
-		panel_1.add(lblWallet);
+		panel1.add(lblWallet);
 		
 		JPanel panel2 = new JPanel();
-		panel2.setBounds(10, 406, 414, 23);
+		panel2.setBounds(TEN, 406, 414, 23);
 		panel2.add(copyright);
 		
-		JButton buyHouse0 = new JButton("Buy");
-		buyHouse0.setBounds(10, 225, 61, 23);
+		buyHouse0 = new JButton("Buy");
+		buyHouse0.setBounds(TEN, 225, 61, 23);
 		buyHouse0.addActionListener(this);
 		buyHouse0.setActionCommand("b0");
 		getFrame().getContentPane().add(buyHouse0);
 		
-		JButton sellHouse0 = new JButton("Sell");
-		sellHouse0.setBounds(10, 259, 61, 23);
+		sellHouse0 = new JButton("Sell");
+		sellHouse0.setBounds(TEN, 259, 61, 23);
 		sellHouse0.addActionListener(this);
 		sellHouse0.setActionCommand("s0");
 		getFrame().getContentPane().add(sellHouse0);
 		
-		JButton sellHouse1 = new JButton("Sell");
+		sellHouse1 = new JButton("Sell");
 		sellHouse1.setBounds(81, 259, 61, 23);
 		sellHouse1.addActionListener(this);
 		sellHouse1.setActionCommand("s1");
 		getFrame().getContentPane().add(sellHouse1);
 		
-		JButton buyHouse1 = new JButton("Buy");
+		buyHouse1 = new JButton("Buy");
 		buyHouse1.setBounds(81, 225, 61, 23);
 		buyHouse1.addActionListener(this);
 		buyHouse1.setActionCommand("b1");
 		getFrame().getContentPane().add(buyHouse1);
 		
-		JButton buyHouse2 = new JButton("Buy");
+		buyHouse2 = new JButton("Buy");
 		buyHouse2.setBounds(148, 225, 61, 23);
 		buyHouse2.addActionListener(this);
 		buyHouse2.setActionCommand("b2");
 		getFrame().getContentPane().add(buyHouse2);
 		
-		JButton sellHouse2 = new JButton("Sell");
+		sellHouse2 = new JButton("Sell");
 		sellHouse2.setBounds(148, 259, 61, 23);
 		sellHouse2.addActionListener(this);
 		sellHouse2.setActionCommand("s2");
 		getFrame().getContentPane().add(sellHouse2);
 		
-		JButton buyHouse3 = new JButton("Buy");
+		buyHouse3 = new JButton("Buy");
 		buyHouse3.setBounds(219, 225, 63, 23);
 		buyHouse3.addActionListener(this);
 		buyHouse3.setActionCommand("b3");
 		getFrame().getContentPane().add(buyHouse3);
 		
-		JButton sellHouse3 = new JButton("Sell");
+		sellHouse3 = new JButton("Sell");
 		sellHouse3.setBounds(219, 259, 63, 23);
 		sellHouse3.addActionListener(this);
 		sellHouse3.setActionCommand("s3");
 		getFrame().getContentPane().add(sellHouse3);
 		
-		JButton buyHouse4 = new JButton("Buy");
+		buyHouse4 = new JButton("Buy");
 		buyHouse4.setBounds(292, 225, 61, 23);
 		buyHouse4.addActionListener(this);
 		buyHouse4.setActionCommand("b4");
 		getFrame().getContentPane().add(buyHouse4);
 		
-		JButton sellHouse4 = new JButton("Sell");
+		sellHouse4 = new JButton("Sell");
 		sellHouse4.setBounds(292, 259, 61, 23);
 		sellHouse4.addActionListener(this);
 		sellHouse4.setActionCommand("s4");
 		getFrame().getContentPane().add(sellHouse4);
 		
-		JButton buyHouse5 = new JButton("Buy");
+		buyHouse5 = new JButton("Buy");
 		buyHouse5.setBounds(363, 225, 61, 23);
 		buyHouse5.addActionListener(this);
 		buyHouse5.setActionCommand("b5");
 		getFrame().getContentPane().add(buyHouse5);
 		
-		JButton sellHouse5 = new JButton("Sell");
+		sellHouse5 = new JButton("Sell");
 		sellHouse5.setBounds(363, 259, 61, 23);
 		sellHouse5.addActionListener(this);
 		sellHouse5.setActionCommand("s5");
 		getFrame().getContentPane().add(sellHouse5);
 		
 		
-		getFrame().getContentPane().add(panel_1);
+		getFrame().getContentPane().add(panel1);
 		getFrame().getContentPane().add(panel);
 		getFrame().getContentPane().add(panel2);
 
 	}
 	
 	@Override
-	public void actionPerformed(ActionEvent e) {
+	public void actionPerformed(final ActionEvent e) {
 		String action = e.getActionCommand();
 		
-		if(action.equals("b0")){
-			if(!getHouse(0).getOwnershipState()){
+		if (action.equals("b0")) {
+			if (!getHouse(0).getOwnershipState()) {
 				model.buyHouse(0);
 			}
 		}
-		if(action.equals("s0")){
-			if(getHouse(0).getOwnershipState()){
+		if (action.equals("s0")) {
+			if (getHouse(0).getOwnershipState()) {
 				model.sellHouse(0);
 			}
 		}
 		
-		if(action.equals("b1")){
-			if(!getHouse(1).getOwnershipState()){
+		if (action.equals("b1")) {
+			if (!getHouse(1).getOwnershipState()) {
 				model.buyHouse(1);
 			}
 		}
-		if(action.equals("s1")){
-			if(getHouse(1).getOwnershipState()){
+		if (action.equals("s1")) {
+			if (getHouse(1).getOwnershipState()) {
 				model.sellHouse(1);
 			}
 		}
 		
-		if(action.equals("b2")){
-			if(!getHouse(2).getOwnershipState()){
+		if (action.equals("b2")) {
+			if (!getHouse(2).getOwnershipState()) {
 				model.buyHouse(2);
 			}
 		}
-		if(action.equals("s2")){
-			if(getHouse(2).getOwnershipState()){
+		if (action.equals("s2")) {
+			if (getHouse(2).getOwnershipState()) {
 				model.sellHouse(2);
 			}
 		}
 		
-		if(action.equals("b3")){
-			if(!getHouse(3).getOwnershipState()){
-				model.buyHouse(3);
+		if (action.equals("b3")) {
+			if (!getHouse(THREE).getOwnershipState()) {
+				model.buyHouse(THREE);
 			}
 		}
-		if(action.equals("s3")){
-			if(getHouse(3).getOwnershipState()){
-				model.sellHouse(3);
-			}
-		}
-		
-		if(action.equals("b4")){
-			if(!getHouse(4).getOwnershipState()){
-				model.buyHouse(4);
-			}
-		}
-		if(action.equals("s4")){
-			if(getHouse(4).getOwnershipState()){
-				model.sellHouse(4);
+		if (action.equals("s3")) {
+			if (getHouse(THREE).getOwnershipState()) {
+				model.sellHouse(THREE);
 			}
 		}
 		
-		if(action.equals("b5")){
-			if(!getHouse(5).getOwnershipState()){
-				model.buyHouse(5);
+		if (action.equals("b4")) {
+			if (!getHouse(FOUR).getOwnershipState()) {
+				model.buyHouse(FOUR);
 			}
 		}
-		if(action.equals("s5")){
-			if(getHouse(5).getOwnershipState()){
-				model.sellHouse(5);
+		if (action.equals("s4")) {
+			if (getHouse(FOUR).getOwnershipState()) {
+				model.sellHouse(FOUR);
+			}
+		}
+		
+		if (action.equals("b5")) {
+			if (!getHouse(FIVE).getOwnershipState()) {
+				model.buyHouse(FIVE);
+			}
+		}
+		if (action.equals("s5")) {
+			if (getHouse(FIVE).getOwnershipState()) {
+				model.sellHouse(FIVE);
 			}
 		}
 		
 		
-		if(action.equals("quit")){
+		if (action.equals("quit")) {
 			System.exit(0);
 		}
-		if(action.equals("reset")){
+		if (action.equals("reset")) {
 			this.frame.dispose();
 			super.reset();
 		}
-		if(action.equals("settings")){
+		if (action.equals("settings")) {
 			System.out.println("Settings");
 		}
 		
@@ -295,10 +334,10 @@ public class GUI extends Init implements ActionListener {
 	}
 	
 	/**
-	 * @param i, index of the house
+	 * @param i : index of the house
 	 * @return the house at index i
 	 */
-	public House getHouse(int i){
+	public House getHouse(final int i) {
 		return model.getHouseList().get(i);
 	}
 	
@@ -311,9 +350,9 @@ public class GUI extends Init implements ActionListener {
 	}
 
 	/**
-	 * @param the frame to set
+	 * @param frameToSet : the frame to set
 	 */
-	public void setFrame(JFrame frame) {
+	public void setFrame(final JFrame frameToSet) {
 		this.frame = frame;
 	}
 	
--- org.game/org/game/IHouse.java
+++ org.game/org/game/IHouse.java
@@ -1,22 +1,30 @@
 package org.game;
 
+/**
+ * 
+ * The House interface.
+ *
+ */
 public interface IHouse {
 	
-	/*
-	 * Whether or not the house is currently for sale
+	/**
+	 * Whether or not the house is currently for sale.
+	 * @return The state of ownership of a house.
 	 */
 	boolean getOwnershipState();
 	
-	/*
-	 * Current price of the house
+	/**
+	 * Current price of the house.
+	 * @return The price of the house.
 	 */
 	double getPrice();
 	
-	/*
+	/**
 	 * Int determining what type of house this is.
 	 * 0: low cost
 	 * 1: med cost
 	 * 2: high cost
+	 * @return The tier of the house.
 	 */
 	int getTier();
 }
